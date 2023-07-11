53bde0fe44c22be02496fdb5a15305ceae8b105d
장현희 <99874673+hhJ830@users.noreply.github.com>
2023-07-11 14:42:02 +0900

    [Silver IV] Title: 큐, Time: 436 ms, Memory: 19700 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,108 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static ArrayList<String> arrayList=new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        String str;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String N = br.readLine();
+        int m=Integer.parseInt(N);
+        for(int i=0;i<m;i++){
+            str= br.readLine();
+
+            String[] ArrayStr=str.split(" ");
+            if(ArrayStr.length==2)
+            {
+                push(ArrayStr);
+            }
+            else
+            {
+                switch (ArrayStr[0])
+                {
+                    case "front":
+                        String front_result=front();
+                        System.out.println(front_result);
+                        break;
+                    case "back":
+                        String back_result=back();
+                        System.out.println(back_result);
+                        break;
+                    case "size":
+                        System.out.println(arrayList.size());
+                        break;
+                    case "empty":
+                        if(arrayList.isEmpty()==true)
+                        {
+                            System.out.println("1");
+                        }
+                        else{
+                            System.out.println("0");
+                        }
+                        break;
+                    case "pop":
+                        String pop_result=pop();
+                        System.out.println(pop_result);
+                        break;
+                    default:
+                        System.out.println("잘못 입력");
+                        break;
+                }
+            }
+        }
+    }
+
+    private static void push(String[] array){
+        arrayList.add(array[1]);
+    }
+
+    private static String front(){ //가장 앞에 있는 정수 출력
+        int size=arrayList.size();
+        String result="";
+        if(size>0)
+        {
+            result=arrayList.get(0);
+        }
+        else
+        {
+            result="-1";
+        }
+        return result;
+    }
+
+    private static String back(){
+        int size=arrayList.size();
+        String result="";
+        if(size>0)
+        {
+            result=arrayList.get(size-1);
+        }
+        else
+        {
+            result="-1";
+        }
+        return result;
+    }
+    private static String pop(){ //가장 앞 정수 빼고 그 수 출력
+        int size=arrayList.size();
+        String result="";
+        if(size>0){
+            result=arrayList.get(0);
+            arrayList.remove(0);
+        }
+        else{
+            result="-1";
+        }
+
+        return result;
+    }
+}
