6b3e48fd28325ceb1598172ac26f52c233488ecd
장현희 <99874673+hhJ830@users.noreply.github.com>
2023-08-01 23:17:48 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String str=bufferedReader.readLine();
+
+        int A = Integer.parseInt(str.split(" ")[0]);
+        int B = Integer.parseInt(str.split(" ")[1]);
+
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+    }
+}
