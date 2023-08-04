6a58651755a605d43e605436e1cc7d7ddfcb5bc2
장현희 <99874673+hhJ830@users.noreply.github.com>
2023-08-04 17:04:38 +0900

    [Bronze V] Title: Hello World, Time: 116 ms, Memory: 13864 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,6 +1,11 @@
-public class Main{
+import java.io.*;
+import java.nio.Buffer;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
-      
     }
 }
