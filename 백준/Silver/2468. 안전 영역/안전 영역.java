a55850679eb99f090d00820af27dddfbd3b72f01
장현희 <99874673+hh830@users.noreply.github.com>
2025-04-27 20:14:00 +0900

    [Silver I] Title: 안전 영역, Time: 208 ms, Memory: 18772 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(bufferedReader.readLine());
+        map = new int[n][n];
+
+        int max = 0;
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+                max = Math.max(max, map[i][j]);
+            }
+        }
+
+        for (int h = 0; h <= max; h++) {
+            visited = new boolean[n][n];
+            int count = 0;
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (!visited[i][j] && map[i][j] > h) {
+                        dfs(i, j, h);
+                        count++;
+                    }
+                }
+            }
+            result = Math.max(result, count);
+        }
+
+        System.out.println(result);
+    }
+
+    static void dfs(int x, int y, int height) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < n && ny < n) {
+                if (!visited[nx][ny] && map[nx][ny] > height) {
+                    dfs(nx, ny, height);
+                }
+            }
+        }
+    }
+}
